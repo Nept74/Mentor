906964ecf579a51998a88938caaf3c804c91a3c7
Mihail <apanasov.m@yandex.ru>
2023-02-01 21:42:32 +0300

    first hw

---
+++ src/components/AsanaMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+  </template>
+  
+  <style scoped>
+  div {
+    background-color:white;
+    width: 100%;
+    height: calc(100% - 48px);
+  }
+
+  </style>
+  
---
+++ src/components/AsanaSidebar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+  </template>
+  
+  <style scoped>
+  div {
+    float: left;
+    background-color: #424244;
+    width: 240px;
+    height: calc(100% - 48px);
+  }
+
+  </style>
+  
